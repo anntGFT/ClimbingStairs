c0e716c01c1adb074cd435f5cfdd20774facd94d
annt <annt@gft.com>
2022-12-05 11:17:45 +0000

    Done with recursion

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int climbStairs(int n) {
+
+        if(n == 1 ){
+
+            return 1;
+        }
+        if ( n == 2) {
+
+            return 2;
+        }
+
+        n = climbStairs(n - 1) + climbStairs(n - 2);
+
+        return n;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,59 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int result, steps;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyOneStep(){
+
+        steps = 1;
+        result = 1;
+
+        assertEquals(result, solution.climbStairs(steps));
+
+    }
+
+    @Test
+    public void TwoSteps(){
+
+        steps = 2;
+        result = 2;
+
+        assertEquals(result, solution.climbStairs(steps));
+
+    }
+
+    @Test
+    public void ThreeSteps(){
+
+        steps = 3;
+        result = 3;
+
+        assertEquals(result, solution.climbStairs(steps));
+
+    }
+
+    @Test
+    public void TenSteps(){
+
+        steps = 10;
+        result = 89;
+
+        assertEquals(result, solution.climbStairs(steps));
+
+    }
 }
